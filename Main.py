8cf8046bdde59e20431b3eeacf46c724c028bdff
Antony <Antonyrejikurian@gmail.com>
2023-12-29 19:07:11 +1100

    stopped player from moving outside screen

--- Main.py
+++ Main.py
@@ -1,26 +1,42 @@
- #Simple pygame program
-
-# Import and initialize the pygame library
 import pygame
 pygame.init()
 
+screen_height = 250
+screen_width = 500
+
 # Set up the drawing window
-screen = pygame.display.set_mode([500, 500])
+screen = pygame.display.set_mode([screen_width,screen_height ])
+
+# Set up the player object
+player_radius = 25
+player_x, player_y = 250, 250
+player_speed = 0.1
 
 # Run until the user asks to quit
 running = True
 while running:
-
-    # Did the user click the window close button?
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
+    # Get the state of all keys
+    keys = pygame.key.get_pressed()
+
+    # Update the player position based on arrow key input
+    if keys[pygame.K_LEFT] and (player_x -player_speed > 0):
+        player_x -= player_speed
+    if keys[pygame.K_RIGHT] and (player_x + player_speed < screen_width):
+        player_x += player_speed
+    if keys[pygame.K_UP] and (player_y - player_speed > 0):
+        player_y -= player_speed 
+    if keys[pygame.K_DOWN] and (player_y -player_speed < screen_height):
+        player_y += player_speed
+
     # Fill the background with white
     screen.fill((255, 255, 255))
 
-    # Draw a solid blue circle in the center
-    pygame.draw.circle(screen, (0, 0, 255), (250, 250), 75)
+    # Draw the player as a red circle
+    pygame.draw.circle(screen, (255, 0, 0), (int(player_x), int(player_y)), player_radius)
 
     # Flip the display
     pygame.display.flip()
