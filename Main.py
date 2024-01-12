e16532d42da5bbff1e1b1afbd4c57ddee324c0cb
Antony <Antonyrejikurian@gmail.com>
2024-01-12 23:56:00 +1100

    nade snake class

--- Main.py
+++ Main.py
@@ -1,6 +1,7 @@
 import pygame
 import math
 import random
+from pygame.math import Vector2
 
 
 pygame.init()
@@ -16,7 +17,28 @@ class FOOD:
     def draw_food(self):
         food_rect = pygame.Rect(self.pos.x*cell_size,self.pos.y*cell_size,cell_size,cell_size) 
         pygame.draw.rect(screen,(126,123,123),food_rect)
+    
+class Snake: 
+    def __init__(self):
+        self.body = [Vector2(1,2),Vector2(1,3)]
+        self.direction =1 
+    
+    def draw_snake(self):
+        
+        for block in self.body:
+            food_rect = pygame.Rect(block.x*cell_size,block.y*cell_size,cell_size,cell_size) 
+            pygame.draw.rect(screen,(126,0,123),food_rect)
 
+    def move_snake(self):
+        self.body[0]+= Vector2(0,1)
+        prev = self.body[0]
+
+        for block in self.body:
+            copy = block
+            block = prev
+            prev = copy
+
+        self.body[1]=block
 
 #make into a grid
 cell_size = 20
@@ -55,7 +77,7 @@ num_circles =0
 
 #
 food = FOOD()
-
+snake = Snake()
 running = True
 while running:
     for event in pygame.event.get():
@@ -91,22 +113,23 @@ while running:
         right = False
 
     food.draw_food()
-
+    snake.draw_snake()
+    snake.move_snake()
     # Update the player position based on arrow key input
     if left and (player_x -player_speed > 0):
-        player_x -= player_speed
+        player_x -= player_speed*cell_size
         for i in range(num_circles+1):
             pygame.draw.circle(screen, (100, 0,0 ), (int(player_x+i*player_radius), int(player_y)), player_radius)
     if right and (player_x + player_speed < screen_width):
-        player_x += player_speed
+        player_x += player_speed *cell_size
         for i in range(num_circles+1):
             pygame.draw.circle(screen, (100, 0,0 ), (int(player_x-i*player_radius), int(player_y)), player_radius)
     if up and (player_y - player_speed > 0):
-        player_y -= player_speed 
+        player_y -= player_speed *cell_size
         for i in range(num_circles+1):
             pygame.draw.circle(screen, (100, 0,0 ), (int(player_x), int(player_y+i*player_radius)), player_radius)
     if down and (player_y -player_speed < screen_height):
-        player_y += player_speed
+        player_y += player_speed*cell_size
         for i in range(num_circles+1):
             pygame.draw.circle(screen, (100, 0,0 ), (int(player_x), int(player_y-i*player_radius)), player_radius)
 
@@ -116,28 +139,28 @@ while running:
 
 
 
-    if distance < player_radius + circle_radius and new_circle!= True:
+    # if distance < player_radius + circle_radius and new_circle!= True:
        
-        #snakes tail length
-        num_circles += 1
-        #new circle
-        new_circle = True
-    elif new_circle!= True:
-        pygame.draw.circle(screen, (0, 0, 255), (int(circle[0]), int(circle[1])), circle[2])
-    elif new_circle:
-        circle = (random.randint(0, screen_width), random.randint(0, screen_height),
-            15) 
-        new_circle = False
+    #     #snakes tail length
+    #     num_circles += 1
+    #     #new circle
+    #     new_circle = True
+    # elif new_circle!= True:
+    #     pygame.draw.rect(screen, (0, 0, 255), (int(circle[0]), int(circle[1])), circle[2])
+    # elif new_circle:
+    #     circle = (random.randint(0, screen_width), random.randint(0, screen_height),
+    #         15) 
+    #     new_circle = False
 
     
 
     # Draw the player as a red circle
-    pygame.draw.circle(screen, (255, 0, 0), (int(player_x), int(player_y)), player_radius)
+
   
     # Flip the display
     pygame.display.flip()
 
-    clock.tick(60)
+    clock.tick(5)
 
 # Done! Time to quit.
 pygame.quit()
